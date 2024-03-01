b61d1eb1241b8367dc2e9cd0ecafa68cb89de9ed
Ahsanul Amin Shanto <103604442+AhsanulAminShanto@users.noreply.github.com>
2024-03-01 15:50:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function contactForm() {
-    
+function contactUs() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var massage = document.getElementById("massage").value;
+    var displayInputDiv = document.getElementById("displayInput");
+    displayInputDiv.innerHTML = `Successfully massage sent by  ${name} from ${email} `;
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Massage:", massage);
 }
